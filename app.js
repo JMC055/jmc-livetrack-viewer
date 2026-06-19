9e5800aa9c215780b8861ca0fd507fc95273694f
JMC055 <jeanmarcchenal@gmail.com>
2026-06-19 23:25:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const SUPABASE_URL = "https://gshvckevlubgjengwnwf.supabase.co";
 const API_KEY = "sb_publishable_RVd938UzHJMLtn2L-1yxXA_W86zy2TM";
 
+let map = L.map('map').setView([48.8566, 2.3522], 13);
+
+L.tileLayer(
+    'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+    {
+        maxZoom: 17,
+        attribution: 'OpenTopoMap'
+    }
+).addTo(map);
+
+let marker = L.marker([48.8566, 2.3522]).addTo(map);
+
 function formatElapsed(seconds) {
     const h = Math.floor(seconds / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = seconds % 60;
 
-    return `${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(2, '0')}`;
+    return `${String(h).padStart(2,'0')}:${String(m).padStart(2,'0')}:${String(s).padStart(2,'0')}`;
 }
 
 async function fetchData() {
@@ -24,6 +36,7 @@ async function fetchData() {
     const data = await response.json();
 
     if (data.length > 0) {
+
         const session = data[0];
 
         document.getElementById("distance").innerText =
@@ -49,6 +62,13 @@ async function fetchData() {
 
         document.getElementById("lon").innerText =
             Number(session.lon).toFixed(5);
+
+        let lat = Number(session.lat);
+        let lon = Number(session.lon);
+
+        marker.setLatLng([lat, lon]);
+
+        map.setView([lat, lon], map.getZoom());
     }
 }
 
